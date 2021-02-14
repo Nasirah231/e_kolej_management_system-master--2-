ab6348cdfc1840da1b7bf7ba8691ddb28536b2e0
Nasirah231 <sirah23197@hotmail.com>
2021-02-14 23:58:48 +0800

    updated 15/2

--- e_kolej_management_system-master/e_kolej_management_system/e_kolej_management_system/settings.py
+++ e_kolej_management_system-master/e_kolej_management_system/e_kolej_management_system/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'so*rai_2(lk7t(yh%de+_kp_c%*r_b9wkga%gyo5tl9_8_r!xx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 #HEROKU LIVE PROJECT LINK
 ALLOWED_HOSTS = ["ekolejmanagementsystem.herokuapp.com"]
